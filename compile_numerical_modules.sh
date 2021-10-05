9641a25627f664cf605a491a2b8dccfc97e7162c
Albert Rosich <albertrosich@gmail.com>
2021-10-05 18:30:06 +0200

    Add files via upload

--- compile_numerical_modules.sh
+++ compile_numerical_modules.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 cd ./src &&
-
-f2py2.7 -m -c mGLS mGLS.f95  -llapack  &&
-mv mGLS*.so ../bin
-
-f2py2.7 -m -c mMGLS mMGLS.f95 --opt=-O3 -llapack  &&
-f2py2.7 -m -c mMGLS_AR mMGLS_AR.f95 --opt=-O3 -llapack &&
+f2py2.7  -m -c mMGLS mMGLS.f95 --opt=-O3 -llapack  &&
 mv mMGLS*.so ../bin 
